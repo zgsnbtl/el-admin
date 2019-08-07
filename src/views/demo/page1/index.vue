405bb365daba0c2c62dc168c34dd13537a8a7f76
zgsnbtl <443108484@qq.com>
2019-08-07 23:56:16 +0800

    first commit

---
+++ src/views/demo/page1/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <d2-container>
+    <template slot="header">Page 1 header</template>
+    Hello World
+  </d2-container>
+</template>
+
+<script>
+export default {
+  name: 'page1',
+  data () {
+    return {
+    }
+  },
+  created () {
+    this.getlist()
+  },
+  methods: {
+    getlist () {
+      this.$axios.post(`http://t.waiqin.co/api/department/all`,
+        {
+          headers: {
+            ' Authorization': 'xph2edDC8G3dKleo'
+          },
+          query: { withSubDepartments: true, withUsers: true }
+        }
+      ).then(res => {
+        console.log(res)
+      })
+    }
+  }
+}
+</script>
